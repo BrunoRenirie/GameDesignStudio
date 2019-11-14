f7cc8917ecc46cf950c5f130cb026ece9fd9aa0a
Miquel <joellb@hotmail.nl>
2019-11-14 14:18:32 +0100

    fix

---
+++ Assets/Scripts/UI/ToolTip.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool pointerDown;
+    private bool pointerUp;
+    private float pointerDownTimer;
+
+    [SerializeField]
+    private float requiredHoldTime;
+
+    public UnityEvent onLongClick;
+
+    [SerializeField]
+    private Image fillImage;
+    [SerializeField]
+    private GameObject Panel;
+    private Animator animator;
+
+    void Start()
+    {
+             animator = Panel.GetComponent<Animator>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pointerDown = false;
+        Reset();
+    }
+
+    private void Update()
+    {
+        if (pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if (pointerDownTimer >= requiredHoldTime)
+            {
+                if (onLongClick != null)
+                {
+                    onLongClick.Invoke();
+                    bool isOpen = animator.GetBool("open");
+                    animator.SetBool("open", !isOpen);
+                    Reset();
+                }
+            }
+            fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+        }
+    }
+
+    private void Reset()
+    {
+        pointerDown = false;
+        pointerDownTimer = 0;
+        fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+    }
+}
