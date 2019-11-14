c17c38b956803eaa026d98211bac9058e7cbc340
wesselvink <22092@ma-web.nl>
2019-11-14 13:42:46 +0100

    add wessel

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    private Animator animator;
+    public bool spawnPanel = false;
+
+    public void Paus()
+    {
+        if (spawnPanel == false)
+        {
+            SceneManager.LoadSceneAsync(5, LoadSceneMode.Additive);
+            spawnPanel = true;
+        }else
+        {
+          animator =  GameObject.Find("PauzePanel2").GetComponent<Animator>();
+          animator.SetBool("IsOpen", true);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Resume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resume : MonoBehaviour
+{
+    public GameObject pauzePanel;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = pauzePanel.GetComponent<Animator>();
+    }
+
+    public void Doorgaan()
+    {
+        animator.SetBool("IsOpen",  false);
+    }
+}
